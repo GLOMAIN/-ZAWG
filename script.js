a54a1968b3908df016fa29d584f6a3c00087fc75
GLOMAIN <gloryguy440@gmail.com>
2025-01-28 12:25:13 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Example: Add interactivity to the "Buy $ZAWG" button
-document.getElementById('buyButton').addEventListener('click', () => {
-    alert('Redirecting to buy $ZAWG...');
-    // Replace with actual buy link
-    window.location.href = 'https://raydium.io/swap';
-});
